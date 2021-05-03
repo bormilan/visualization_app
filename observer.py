f5b227da083bf1329078591ab1c6d71bd1331f58
bormilan <milanbor66@gmail.com>
2021-05-03 15:21:26 +0200

    column observer

--- observer.py
+++ observer.py
@@ -18,22 +18,24 @@ class Summary(Observer):
 		super().__init__(Subject)
 		self.mode = 0
 
-		#Buttons
-		self.title1 = tk.Button(text="Head")
-		self.title1.config(width=8,height=2)
-		self.title1.config(font=("Courier", 15))
-		self.title1.place(x=1000-520,y=700-180-70)		
-		self.title1.config(relief="groove")		
-		self.title1.config(command=lambda: self.setMode(0))
+		#---MODE BUTTONS---#
+		#1
+		self.head = tk.Button(text="Head")
+		self.head.config(width=8,height=2)
+		self.head.config(font=("Courier", 15))
+		self.head.place(x=1000-520,y=700-180-70)		
+		self.head.config(relief="groove")		
+		self.head.config(command=lambda: self.setMode(0))
 
-		self.title2 = tk.Button(text="Describe")
-		self.title2.config(width=8,height=2)
-		self.title2.config(font=("Courier", 15))
-		self.title2.place(x=1000-400,y=700-180-70)		
-		self.title2.config(relief="groove")
-		self.title2.config(command=lambda: self.setMode(1))
+		#2
+		self.describe = tk.Button(text="Describe")
+		self.describe.config(width=8,height=2)
+		self.describe.config(font=("Courier", 15))
+		self.describe.place(x=1000-400,y=700-180-70)		
+		self.describe.config(relief="groove")
+		self.describe.config(command=lambda: self.setMode(1))
 
-		#Label
+		#---CONTENT LABEL---#
 		self.SummaryLabel = tk.Label()
 		self.SummaryLabel.place(x=1000-520,y=700-180)
 		self.SummaryLabel.config(bg="thistle2")
@@ -43,11 +45,11 @@ class Summary(Observer):
 	def update(self): 		
 		#Button colors
 		if self.mode == 0:
-			self.title1.config(bg="tomato3")
-			self.title2.config(bg="dim gray")
+			self.head.config(bg="tomato3")
+			self.describe.config(bg="dim gray")
 		else:
-			self.title1.config(bg="dim gray")
-			self.title2.config(bg="tomato3")
+			self.head.config(bg="dim gray")
+			self.describe.config(bg="tomato3")
 
 		self.SummaryLabel.config(text=self.getSummary())
 
@@ -72,25 +74,46 @@ class Column(Observer):
 	def __init__(self, Subject):
 		super().__init__(Subject)
 		self.column = ""
+		self.mode = 0
 
+		#---CONTENT LABEL---#
 		self.columnLabel = tk.Label()
 		self.columnLabel.config(text="nincs oszlop kiválasztva")
-		self.columnLabel.place(x=10,y=520)
+		self.columnLabel.place(x=10,y=600)
 		self.columnLabel.config(bg="thistle2")
 		self.columnLabel.config(relief="solid")
-		self.columnLabel.config(width=40,height=10)
+		self.columnLabel.config(width=20,height=5)
 
+		#---COLUMN NAME ENTRY---#
 		self.entry = tk.Entry(bg="cyan3")
-		self.entry.place(x=10,y=480,width=100,height=30)
+		self.entry.place(x=10,y=560,width=100,height=30)
 		self.entry.bind('<Return>',(lambda event: self.setColumn(self.entry.get())))
 
+		#---OK BUTTON---#
 		self.button = tk.Button(text="Ok")
-		self.button.config(width=8,height=2)
+		self.button.config(width=3,height=1)
 		self.button.config(font=("Courier", 15))
-		self.button.place(x=120,y=450)		
+		self.button.place(x=120,y=550)		
 		self.button.config(relief="groove")
 		self.button.config(bg="tomato3")
 		self.button.config(command=lambda: self.setColumn(self.entry.get()))
+
+		#---MODE BUTTONS---#
+		#1
+		self.sum = tk.Button(text="Sum")
+		self.sum.config(width=6,height=1)
+		self.sum.config(font=("Courier", 15))
+		self.sum.place(x=180,y=600)		
+		self.sum.config(relief="groove")		
+		self.sum.config(command=lambda: self.setMode(0))
+
+		#2
+		self.type = tk.Button(text="type")
+		self.type.config(width=6,height=1)
+		self.type.config(font=("Courier", 15))
+		self.type.place(x=180,y=650)		
+		self.type.config(relief="groove")
+		self.type.config(command=lambda: self.setMode(1))
 
 	def setColumn(self,column):
 		self.column = column
@@ -98,9 +121,30 @@ class Column(Observer):
 
 	def update(self):	
 		try:
-			sum = np.sum(self.subject.getState()[self.column])
-			self.columnLabel.config(text=sum)
+			self.columnLabel.config(text=self.getColumn())
 		except KeyError:
 			self.columnLabel.config(text="nincs oszlop kiválasztva")
 		except TypeError:
 			self.columnLabel.config(text="az oszlop adatai nem megfelelőek")
+
+		#Button colors
+		if self.mode == 0:
+			self.sum.config(bg="tomato3")
+			self.type.config(bg="dim gray")
+		else:
+			self.sum.config(bg="dim gray")
+			self.type.config(bg="tomato3")
+
+	def setMode(self,newMode):
+		if newMode == 0:
+			self.mode = 0
+			self.update()
+		else:
+			self.mode = 1
+			self.update()
+
+	def getColumn(self):
+		if self.mode == 0:	
+			return np.sum(self.subject.getState()[self.column])
+		else:
+			return self.subject.getState()[self.column].dtypes
